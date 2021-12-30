44267abb66b2d295d4547cbbfee4e9a2216a9c1c
Dannie Isaac Winford <dannie.winford@gmail.com>
2021-12-30 10:17:20 +0530

    Added two view functions

--- greet/views.py
+++ greet/views.py
@@ -1,3 +1,9 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return HttpResponse("<h3>A random index page</h3>")
+
+def sayHello(request, name):
+    return HttpResponse(f"<h3>Hello {name}</h3>")
